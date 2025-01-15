91201fef72936829c17756d9d9bf93ddc026bded
Romain Lambert <lambertromain66270@gmail.com>
2025-01-15 15:09:40 +0100

    m6 correction

--- module_06/ex00/ScalarConverter.hpp
+++ module_06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: romlambe <romlambe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 16:27:07 by romlambe          #+#    #+#             */
-/*   Updated: 2025/01/13 10:23:55 by romlambe         ###   ########.fr       */
+/*   Updated: 2025/01/15 15:02:33 by romlambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 #define SCALAR_HPP
 
 #include <iostream>
-#include <cstdlib>
+#include <string>
 #include <cmath>
+#include <climits>
 
 class ScalarConverter{
-public:
+private:
 	ScalarConverter();
 	ScalarConverter(ScalarConverter const &src);
 	~ScalarConverter();
@@ -26,6 +27,8 @@ public:
 	ScalarConverter &operator=(ScalarConverter const &other);
 
 	std::string _str;
+
+public:
 
 	static void converter(std::string const &str);
 };
--- module_06/ex01/Serializer.hpp
+++ module_06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: romlambe <romlambe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 15:21:47 by romlambe          #+#    #+#             */
-/*   Updated: 2025/01/15 14:15:04 by romlambe         ###   ########.fr       */
+/*   Updated: 2025/01/15 15:07:12 by romlambe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ private:
 	Serializer(Serializer const &src);
 	~Serializer();
 	Serializer & operator=(Serializer const &other);
+
+	std::string src;
 public:
 	static uintptr_t serialize(Data* ptr);
 	static Data* deserialize(uintptr_t raw);
